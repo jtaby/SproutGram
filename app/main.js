54bbc3edf3ab3470ea732fdd64a79993537968d9
Majd Taby <mtaby@me.com>
2011-08-19 07:37:39 -0700

    some refactoring, added curtain and centering

--- app/main.js
+++ app/main.js
@@ -17,88 +17,6 @@ SproutGram.PhotosView = SC.CollectionView.extend({
   contentBinding: 'SproutGram.photosController'
 });
 
-SproutGram.PhotoView = SC.View.extend({
-  scale: 1,
-  translateZ: 0,
-  translate: { x: 0, y: 0 },
-  
-  touchStart: function(evt) {
-    this.$().css('z-index',10);
-  },
-  
-  touchEnd: function(evt) {
-  },
-
-  pinchStart: function(recognizer) {
-    console.log("pinchstart" + this.toString());
-    console.log(this.$()[0].style.cssText);
-  },
-
-  pinchChange: function(recognizer) {    
-    this.$().css('scale',function(index, value) {
-      return recognizer.get('scale') * value
-    });
-  },
-
-  pinchEnd: function(recognizer) {
-    this._resetTransforms();
-    //var velocity = recognizer.get('velocity');
-
-    //if (velocity < 0) {
-      //this.$().css('WebkitTransition','-webkit-transform 0.3s ease-out');
-      //this.scale = 1;
-      //this.translate = {x:0,y:0};
-      //this._applyTransforms();
-
-      //var self=this;
-      //window.setTimeout(function() {
-        //this.$().css('WebkitTransition','');
-      //}, 30);
-    //}
-  },
-
-  panOptions: {
-    numberOfRequiredTouches: 2
-  },
-
-  panChange: function(recognizer) {
-    var val = recognizer.get('translation');
-  
-    this.$().css({
-      translateX: '%@=%@'.fmt((val.x < 0)? '-' : '+',Math.abs(val.x)),
-      translateY: '%@=%@'.fmt((val.y < 0)? '-' : '+',Math.abs(val.y))
-    });
-  },
-
-  panEnd: function() {
-    this._resetTransforms();
-  },
-
-  _resetTransforms: function() {
-    var self = this;
-    
-    this.$().animate({
-      scale: 1,
-      translateX: 0,
-      translateY: 0
-    }, 300, function() { self.$().css('z-index',1); })
-  },
-
-  didInsertElement: function() {
-    var canvas = this.$('canvas.image_canvas')[0];
-    if (!canvas) return;
-
-    var image = new Image();
-    image.src = this.getPath('parentView.content.standard_res');
-
-    image.onload = function() {
-      var ctx = canvas.getContext('2d');
-
-      ctx.drawImage(image,50,50,500,500,0,0,256,256);
-    }
-  }
-});
-
 $(document).ready(function() {
 
   $.ajax({
@@ -113,7 +31,8 @@ $(document).ready(function() {
 
         SproutGram.photosController.pushObject(SproutGram.PhotoModel.create({
           standard_res: image.images.standard_resolution.url,
-          username: image.user.username
+          username: image.user.username,
+          itemIndex: i
         }));
       }
     }
---
+++ app/views/photo.js
@@ -0,0 +1,127 @@
+
+var numCols = 4;
+var pictureDimension = 256;
+
+SproutGram.PhotoView = SC.View.extend({
+  
+  itemIndex: null,
+  
+  touchStart: function(evt) {
+    this.$().css('z-index',10);
+    $('#curtain').css('z-index',2);
+  },
+  
+  tapEnd: function() {
+    this._centerPhoto();
+  },
+
+  pinchChange: function(recognizer) {    
+    var jq = this.$();
+    var newScale = recognizer.get('scale');
+    var curScale = jq.css('scale');
+    
+    var boundedScale = Math.max(1,Math.min(2, curScale * newScale));
+    $('#curtain').css('opacity',boundedScale-1);
+    
+    this.$().css('scale',function(index, value) {
+      return newScale * value
+    });
+  },
+
+  pinchEnd: function(recognizer) {
+    var velocity = recognizer.get('velocity');
+    
+    if (velocity > 0) {
+      this._centerPhoto();
+    }
+    else {
+      this._resetTransforms();
+    }
+  },
+
+  panOptions: {
+    numberOfRequiredTouches: 2
+  },
+
+  panChange: function(recognizer) {
+    var val = recognizer.get('translation');
+  
+    this.$().css({
+      translateX: '%@=%@'.fmt((val.x < 0)? '-' : '+',Math.abs(val.x)),
+      translateY: '%@=%@'.fmt((val.y < 0)? '-' : '+',Math.abs(val.y))
+    });
+  },
+
+  _resetTransforms: function(accepted) {
+    var self = this;
+    var position = this._resetPosition();
+    
+    $('#curtain').animate({
+      opacity:0
+    },300);
+    
+    this.$().animate({
+      scale: 1,
+      translateX: 0,
+      translateY: 0,
+      top: position.top,
+      left: position.left,
+      width: position.width,
+      height: position.height,
+    }, 300, function() { 
+      $('#curtain').css('z-index',0);
+      self.$().css('z-index',1);
+    })
+  },  
+  
+  _centerPhoto: function() {
+      
+    $('#curtain').animate({
+      opacity:1
+    },300);
+      
+    this.$().animate({
+      scale: 2,
+      translateX: 0,
+      translateY: 0,
+      
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    }, 300)
+  },
+  
+  willInsertElement: function() {
+    this.$().css(this._resetPosition());
+  },
+  
+  _resetPosition: function() {
+    var el = this.$();
+    var idx = this.getPath('parentView.content.itemIndex');
+    
+    var row = Math.floor(idx / numCols);
+    var col = idx % numCols;
+    
+    return {
+      top: row * pictureDimension,
+      left: col * pictureDimension,
+      width: pictureDimension,
+      height: pictureDimension
+    };
+  },
+
+  didInsertElement: function() {
+    var canvas = this.$('canvas.image_canvas')[0];
+    if (!canvas) return;
+
+    var image = new Image();
+    image.src = this.getPath('parentView.content.standard_res');
+
+    image.onload = function() {
+      var ctx = canvas.getContext('2d');
+
+      ctx.drawImage(image,50,50,500,500,0,0,pictureDimension,pictureDimension);
+    }
+  }
+});
